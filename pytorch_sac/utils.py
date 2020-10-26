3187c5ae3d1452feb3326504a85f2bb8aebb877e
Luis Pineda <luisen.p@gmail.com>
2020-10-26 10:37:30 -0400

    added option to use gym environment

--- pytorch_sac/utils.py
+++ pytorch_sac/utils.py
@@ -13,18 +13,22 @@ import dmc2gym
 
 
 def make_env(cfg):
-    """Helper function to create dm_control environment"""
-    if cfg.env == 'ball_in_cup_catch':
-        domain_name = 'ball_in_cup'
-        task_name = 'catch'
-    else:
-        domain_name = cfg.env.split('_')[0]
-        task_name = '_'.join(cfg.env.split('_')[1:])
+    """Helper function to create dm_control or gym environment"""
 
-    env = dmc2gym.make(domain_name=domain_name,
-                       task_name=task_name,
-                       seed=cfg.seed,
-                       visualize_reward=True)
+    if "gym___" in cfg.env:
+        env = gym.make(cfg.env.split("___")[1])
+    else:
+        if cfg.env == 'ball_in_cup_catch':
+            domain_name = 'ball_in_cup'
+            task_name = 'catch'
+        else:
+            domain_name = cfg.env.split('_')[0]
+            task_name = '_'.join(cfg.env.split('_')[1:])
+
+        env = dmc2gym.make(domain_name=domain_name,
+                           task_name=task_name,
+                           seed=cfg.seed,
+                           visualize_reward=True)
     env.seed(cfg.seed)
     assert env.action_space.low.min() >= -1
     assert env.action_space.high.max() <= 1
